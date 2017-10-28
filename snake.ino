f9c1190ff64b70c59b1cbeef78da149eb4bfd0dd
mrhell007 <>
2017-10-28 20:54:49 +0200

    initial commit

---
+++ snake.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4 // not used / nicht genutzt bei diesem Display
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup()   {                
+
+  // initialize with the I2C addr 0x3C / mit I2C-Adresse 0x3c initialisieren
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
+  // random start seed / zufälligen Startwert für Random-Funtionen initialisieren
+  randomSeed(analogRead(0));
+}
+
+#define DRAW_DELAY 118
+#define D_NUM 47
+
+void loop() {
+  
+  display.clearDisplay();
+  
+  // set text color / Textfarbe setzen
+  display.setTextColor(WHITE);
+  // set text size / Textgroesse setzen
+  display.setTextSize(2);
+  display.setCursor(36,6);
+  display.println("SNAKE");
+  display.setTextSize(1);
+  display.setCursor(41,22);
+  display.println("The Game");
+  display.display();
+  delay(8000);
+  display.clearDisplay();
+  // invert the display / Display invertieren
+  display.invertDisplay(true);
+  delay(2000); 
+  display.invertDisplay(false);
+  delay(1000); 
+  
+}
+
+
+
